1370454392627eb0c8ef8d5da5cb96ee0337d482
Quin Kennedy <quinkennedy@gmail.com>
2012-10-31 21:28:45 -0400

    notifies the user if they try to use non-implemented key words.

--- homespring.js
+++ homespring.js
@@ -12,6 +12,10 @@ var Salmon = require('./salmon'),
 	Shallows = require('./shallows'),
 	ForceField = require('./forcefield');
 
+var NotImplemented = function(){};
+NotImplemented.prototype = new Node();
+NotImplemented.prototype.constructor = NotImplemented;
+
 var Homespring = function(){
 	this.log = false;
 };
@@ -29,8 +33,10 @@ Homespring.prototype.parse = function(a_sText){
 	}
 
 	a_sText = a_sText.replace(/\r\n/g,"\n");
-	if (this.log)
+	if (this.log){
+		console.log("INPUT:");
 		console.log(a_sText);
+	}
 	//first lets convert to all our tokens
 	//read into current token until we see a space not followed by a '.' or a newline
 	var currChar, nextChar;
@@ -80,51 +86,110 @@ Homespring.prototype.parse = function(a_sText){
 		}
 	};
 
-	if (this.log)
+	if (this.log){
+		console.log("TOKENS:");
 		console.log(tokens);
+	}
 
 	//build the program tree (river network)
+	//if we hit a token that creates an undefined node, warn the user
+	//NOTE: excerpt from hstut.html:
+	//		If the parser can't move down any more, the blank token is just treated like a normal token
+	// this suggests that when you are at the root and receive a blank token, you add it as a spring with the name ''
 	var currParent, currNode;
 	for (var i = 0; i < tokens.length; i++) {
 		currToken = tokens[i];
 		if (Program.root == undefined){
-			if (currToken.length == 0){
-				continue;
-			} else {
-				currNode = this.getNode(currToken);
-				Program.root = currNode;
-			}
+			currNode = this.getNode(currToken);
+			Program.root = currNode;
 		} else {
-			if (currToken.length == 0){
-				if (currParent.downstream != undefined){
-					currParent = currParent.downstream;
-				}
+			if (currToken.length == 0 && currParent.downstream != undefined){
+				currParent = currParent.downstream;
 				continue;
 			} else {
 				currNode = this.getNode(currToken);
 				currParent.addUpstream(currNode);
 			}
 		}
+		if (currNode instanceof NotImplemented){
+			//we have not implemented this node type yet
+			console.log('"'+currToken+'" has not been implemented yet');
+			Program.quit = true;
+		}
 		currNode.downstream = currParent;
 		currParent = currNode;
 		currNode = undefined;
 	};
 
-	if (this.log)
+	if (this.log){
+		console.log("TREE:");
 		Program.root.logTree();
+	}
 };
 
 Homespring.prototype.nodeMap = {
-	"bear":Bear,
-	"marshy":Marshy,
-	"universe":Universe,
 	"hatchery":Hatchery,
-	"powers":Powers,
-	"snowmelt":Snowmelt,
 	"hydro power":HydroPower,
-	"sense":Sense,
+	"snowmelt":Snowmelt,
 	"shallows":Shallows,
-	"force field":ForceField
+	"rapids":NotImplemented,//Young salmon take two turns to pass through.
+	"append down":NotImplemented,/*For each downstream salmon that did not arrive from the first child, destroy
+that salmon and append its name to each downstream salmon that did arrive
+from the first child.*/
+	"bear":Bear,
+	"force field":ForceField,
+	"sense":Sense,
+	"clone":NotImplemented,//For each salmon, create a young, downstream salmon with the same name.
+	"young bear":NotImplemented,/*Eats every other mature salmon (the first mature salmon gets eaten, the second
+one doesn’t, etc.). Young salmon are moved to the beginning of the list because
+they don’t have to take the time to evade the bear.*/
+	"bird":NotImplemented,//Eats young salmon.
+	"upstream killing device":NotImplemented,/*When powered and if it contains more than one child, kills all the salmon in the
+last child.*/
+	"waterfall":NotImplemented,//Blocks upstream salmon
+	"universe":Universe,
+	"powers":Powers,
+	"marshy":Marshy,
+	"insulated":NotImplemented,//Blocks power.
+	"upstream sense":NotImplemented,//Blocks the flow of electricity when upstream, mature salmon are present.
+	"downstream sense":NotImplemented,//Blocks the flow of electricity when downstream, mature salmon are present.
+	"evaporates":NotImplemented,//Blocks water and snowmelt when powered.
+	"youth fountain":NotImplemented,//Makes all salmon young.
+	"oblivion":NotImplemented,/*When powered, changes the name of each salmon to “”. Can be destroyed by
+snowmelt.*/
+	"pump":NotImplemented,//Very blocks salmon unless powered.
+	"range sense":NotImplemented,//Blocks electricity when mature salmon are here or upstream.
+	"fear":NotImplemented,//Very blocks salmon when powered.
+	"reverse up":NotImplemented,/*For each downstream salmon that arrived from the second child, move it to the
+first child unless it is prevented from moving there.*/
+	"reverse down":NotImplemented,/*For each downstream salmon that arrived from the first child, move it to the
+second child unless it is prevented from moving there.*/
+	"time":NotImplemented,//Makes all salmon mature
+	"lock":NotImplemented,//Very blocks downstream salmon and blocks snowmelt when powered.
+	"inverse lock":NotImplemented,//Very blocks downstream salmon and blocks snowmelt when not powered.
+	"young sense":NotImplemented,//Blocks electricity when young salmon are present.
+	"switch":NotImplemented,//Blocks electricity unless mature salmon are present.
+	"young switch":NotImplemented,//Blocks electricity unless young salmon are present.
+	"narrows":NotImplemented,//Very blocks salmon if another salmon is present.
+	"append up":NotImplemented,/*For each downstream salmon that did not arrive from the first child, destroy
+that salmon and append its name to each upstream salmon.*/
+	"young range sense":NotImplemented,//Blocks electricity when young salmon are here or upstream.
+	"net":NotImplemented,//Very blocks mature salmon
+	"force down":NotImplemented,/*For each downstream salmon that arrived from the first child, move it to the
+second child unless it is prevented from moving there.
+Also blocks upstream salmon from moving to the last child.*/
+	"force up":NotImplemented,/*For each downstream salmon that arrived from the second child, move it to the
+first child unless it is prevented from moving there.
+Also blocks upstream salmon from moving to the first child.*/
+	"spawn":NotImplemented,//When powered, makes all salmon upstream spawn.
+	"power invert":NotImplemented,/*This node is powered if and only if none of its children are powered. Can be
+destroyed by snowmelt.*/
+	"current":NotImplemented,//Very blocks young salmon
+	"bridge":NotImplemented,//If destroyed by snowmelt, blocks snowmelt and water and very blocks salmon.
+	"split":NotImplemented,/*Splits each salmon into a new salmon for each letter in the original salmon’s
+name. The original salmon are destroyed.*/
+	"range switch":NotImplemented,//Blocks electricity unless mature salmon are here or upstream.
+	"young range switch":NotImplemented//Blocks electricity unless young salmon are here or upstream.
 };
 
 Homespring.prototype.getNode = function(a_sName){
