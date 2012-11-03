28c0ff3ae36d4444a4f28bbe2d18e8ec8dd6a7bb
Quin Kennedy <quinkennedy@LAB-macbookpro-02.(none)>
2012-11-03 04:43:03 -0400

    added an example that prints LinkedList NYC and exits

---
+++ examples/print_linkedlist_nyc.hs
@@ -0,0 +1,14 @@
+Universe has narrows enough split bear hatchery powers Lortimer       
+narrows enough split bear hatchery powers iceland       
+narrows enough split bear hatchery powers newlyweds       
+narrows enough split bear hatchery powers kindred       
+narrows enough split bear hatchery powers endive       
+narrows enough split bear hatchery powers dovetail       
+narrows enough split bear hatchery powers Lucifer       
+narrows enough split bear hatchery powers ingest       
+narrows enough split bear hatchery powers silence       
+narrows enough split bear hatchery powers tentative       
+narrows split enough bear hatchery powers bogus.        
+narrows enough split bear hatchery powers Nosferatu       
+narrows enough split bear hatchery powers Yellowed       
+narrows enough split young. bear hatchery power. invert Calcification,  why all the quit snowmelt ?
